001e54252c5d4ffa31ed2159c9ee30e46c0eee72
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2022-01-27 19:22:08 +0530

    ➡️ Created the redirecting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,47 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Navigation from './components/shared/Navigation/Navigation';
 import Home from './pages/Home/Home';
 import Register from './pages/Register/Register';
 import Login from './pages/Login/Login'
+import Authenticate from './pages/Authenticate/Authenticate';
+
+const isAuth = true;
 
 function App() {
   return (
     <BrowserRouter>
       <Navigation />
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/register' element={<Register/>} />
-        <Route path='/login' element={<Login/>} />
-      </Routes>   
+      <Switch>
+        <GuestRoute path='/' exact>
+          <Home />
+        </GuestRoute>
+        <GuestRoute path='/authenticate'>
+          <Authenticate />
+        </GuestRoute>
+      </Switch>   
     </BrowserRouter>
   );
+}
+
+const GuestRoute = ({ children, ...rest}) => {
+  return (
+    <Route {...rest}
+    render={({ location }) => {
+      return isAuth ? (
+      <Redirect to={
+        {
+          pathname: '/rooms',
+          state: { from: location },
+        }
+      }/>
+      ) : (
+        children
+      )
+    }}>
+
+    </Route>
+  )
 }
 
 export default App;
