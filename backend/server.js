6102257ea499e4f9486b5403639681e2e7e7312e
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2022-02-18 20:00:21 +0530

    ⚒️ Added ice-candidate, session-description, remove-peer, and leave functionality

--- backend/server.js
+++ backend/server.js
@@ -46,16 +46,70 @@ io.on('connection', (socket) => {
         const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
         // when room already exists, send request to clients
         clients.forEach(clientId => {
-            io.to(clientId).emit(ACTIONS.ADD_PEER, {});
+            io.to(clientId).emit(ACTIONS.ADD_PEER, {
+                peerId: socket.id,
+                createOffer: false,
+                user
+            });
+
+            // send request to self to join the room
+            socket.emit(ACTIONS.ADD_PEER, {
+                peerId: clientId,
+                createOffer: true,
+                user:  socketUserMapping[clientId],
+            });
         });
-        // send request to self to join the room
-        socket.emit(ACTIONS.ADD_PEER, {});
+        
         // join room
         socket.join(roomId);
         
         // console.log(clients);
     });
+
+    // Handle Relay Ice
+    socket.on(ACTIONS.RELAY_ICE, ({peerId, icecandidate}) => {
+        io.to(peerId).emit(ACTIONS.ICE_CANDIDATE, {
+            peerId: socket.id,
+            icecandidate,
+        })
+    })
     
+    // Handle Relay SDP - session description
+    socket.on(ACTIONS.RELAY_SDP, ({peerId, sessionDescription}) => {
+        io.to(peerId).emit(ACTIONS.SESSION_DESCRIPTION, {
+            peerId: socket.id,
+            sessionDescription,
+        })
+    })
+
+    // Leaving the Room
+    const leaveRoom = ({roomId}) => {
+        const {rooms} = socket;
+
+        Array.from(rooms).forEach(roomId => {
+            const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+            clients.forEach(clientId => {
+                // Sending to clients to remove
+                io.to(clientId).emit(ACTIONS.REMOVE_PEER, {
+                    peerId: socket.id, 
+                    userId: socketUserMapping[socket.id]?.id,
+                })
+
+                // Sending to self to remove
+                socket.emit(ACTIONS.REMOVE_PEER, {
+                    peerId: clientId,
+                    userId: socketUserMapping[clientId]?.id,
+                })
+            })
+
+            socket.leave(roomId);
+        })
+
+        delete socketUserMapping[socket.id];
+    }
+
+    socket.on(ACTIONS.LEAVE, leaveRoom);
+    socket.on('disconnecting', leaveRoom);
 });
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- frontend/src/actions.js
+++ frontend/src/actions.js
@@ -2,4 +2,9 @@ export const ACTIONS = {
     JOIN: 'join',
     LEAVE: 'leave',
     ADD_PEER: 'add-peer',
+    RELAY_ICE: 'relay-ice',
+    RELAY_SDP: 'relay-sdp',
+    ICE_CANDIDATE: 'ice-candidate',
+    SESSION_DESCRIPTION: 'session-description',
+    REMOVE_PEER: 'remove-peer',
 }
